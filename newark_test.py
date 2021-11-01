6cdfb4d1ef53acee1c9fb94c80a97538a93db44b
wilyub <68028075+wilyub@users.noreply.github.com>
2021-10-31 21:38:12 -0400

    new data

--- newark_test.py
+++ newark_test.py
@@ -28,11 +28,12 @@ class Vehicle:
 
 #Encapsulates all info about a specific scooter (device id)
 class Vehicle_Collection:
-    def __init__(self, dev_id, type):
+    def __init__(self, dev_id, type, provider_name):
         self.dev_id = dev_id
         self.type = type
+        self.provider_name = provider_name
         self.trip_list = []
-        self.total_distance = 0 #Will be added onto later
+        self.total_distance = 0 #Will be added onto later, in km (units)
         self.duration_seconds = None #Will be overwritten later
         self.duration_days = None #Will be overwritten later, = duration_seconds/(60*60*24)
         self.usage_times = None #Will be overwritten later
@@ -51,7 +52,7 @@ def collection_list_update(collection_list, veh_object):
             collection.trip_list.append(veh_object)
             break
     else:
-        collection_new = Vehicle_Collection(veh_object.dev_id, veh_object.type)
+        collection_new = Vehicle_Collection(veh_object.dev_id, veh_object.type, veh_object.provider_name)
         collection_new.trip_list.append(veh_object)
         collection_list = np.append(collection_list, collection_new)
     return collection_list
@@ -69,6 +70,8 @@ def parse_csv(filename, collection_list):
         csvreader = csv.reader(csvfile)
         next(csvreader)
         for row in csvreader:
+            if len(row) == 0:
+                continue
             veh = row_to_obj(row)
             collection_list = collection_list_update(collection_list, veh) 
     return collection_list
@@ -106,11 +109,15 @@ def usage_times(collection_list):
     print("Usage time is finished.")
     return collection_list
 
+#Data is in meters, convert to km
 def distance_traveled(collection_list):
     for collection in collection_list:
         trip_list = collection.trip_list
         for trip in trip_list:
-            collection.total_distance += trip.distance
+            if (trip.distance/1000) > 20: #Threshold of 20km to remove outliers, can try higher thresholds
+                collection.total_distance += 0
+                continue
+            collection.total_distance += (trip.distance/1000)
     print("Distance traveled is finished.")
     return collection_list
 
@@ -151,7 +158,7 @@ def organize_data(collection_list):
 
 #Helper method for plotting cdf. input_data is a list of data (should be numeric). 
 #output_name is a string to identify the figure. Do not include "_cdf.png" in the output_name string.
-def plot_cdf(input_data, output_name):
+def plot_cdf(input_data, output_name, title_str, x_label):
     if len(input_data) == 0:
         return
     count, bins_count = np.histogram(input_data, bins=10)
@@ -160,6 +167,9 @@ def plot_cdf(input_data, output_name):
     plt.plot(bins_count[1:], pdf, color="red", label="PDF")
     plt.plot(bins_count[1:], cdf, label="CDF")
     plt.legend()
+    plt.title(title_str)
+    plt.xlabel(x_label)
+    plt.ylabel("Probability (%)")
     plt.savefig(output_name + "_cdf.png")
     plt.clf()
 
@@ -176,7 +186,7 @@ def cdf_usage_times(collection_list):
             usage_other.append(collection.usage_times)
     #Only scooter data atm
     #plot_cdf(usage_both, "usage_both")
-    plot_cdf(usage_scooter, "usage_scooter")
+    plot_cdf(usage_scooter, "usage_scooter", "CDF of # of Trips per Scooter", "# of Trips")
     #plot_cdf(usage_other, "usage_other")
 
 
@@ -191,10 +201,10 @@ def cdf_distance_traveled(collection_list):
         else:
             distance_other.append(collection.total_distance)
     #plot_cdf(distance_both, "distance_both")
-    plot_cdf(distance_scooter, "distance_scooter")
+    plot_cdf(distance_scooter, "distance_scooter", "CDF of Total Distance Traveled per Scooter", "Distance (km)")
     #plot_cdf(distance_other, "distance_other")
 
-#Plots the cdf of working time in terms of slots (1 slot == 15 minutes)
+#Plots the cdf of working time in terms of days
 def cdf_working_time(collection_list):
     working_both = []
     working_scooter = []
@@ -206,7 +216,7 @@ def cdf_working_time(collection_list):
         else:
             working_other.append(collection.working_time_days)
     #plot_cdf(working_both, "working_both_slots")
-    plot_cdf(working_scooter, "working_scooter_days")
+    plot_cdf(working_scooter, "working_scooter_days", "CDF of Total Active Travel Time per Scooter", "Time (Days)")
     #plot_cdf(working_other, "working_other_slots")
 
 #Plots the cdf of lifetime of vehicle (both working and idle days) in days
@@ -221,7 +231,7 @@ def cdf_duration_days(collection_list):
         else:
             duration_other.append(collection.duration_days)
     #plot_cdf(duration_both, "duration_both_days")
-    plot_cdf(duration_scooter, "duration_scooter_days")
+    plot_cdf(duration_scooter, "duration_scooter_days", "CDF of Total Lifetime per Scooter", "Time (Days)")
     #plot_cdf(duration_other, "duration_other_days")
 
 #Plots the cdf of utilization as a percent (0.5 -> 50% utilization)
@@ -236,12 +246,12 @@ def cdf_utilization(collection_list):
         else:
             utilization_other.append(collection.utilization)
     #plot_cdf(utilization_both, "utilization_both")
-    plot_cdf(utilization_scooter, "utilization_scooter")
+    plot_cdf(utilization_scooter, "utilization_scooter", "CDF of Utilization of Scooter (Travel Time / Lifetime)", "Percent (0.5 = 50%)")
     #plot_cdf(utilization_other, "utilization_other")
 
 #Plots all of the cdfs we want. Only use after organize_data()
 def graph_functions(collection_list):
-    gps_parse(collection_list)
+    #gps_parse(collection_list)
 
 
     cdf_usage_times(collection_list)
@@ -250,11 +260,37 @@ def graph_functions(collection_list):
     cdf_duration_days(collection_list)
     cdf_utilization(collection_list)
 
+def statistics(collection_list):
+    print("Number of vehicles: ", len(collection_list))
+
+    types = []
+    for collection in collection_list:
+        if collection.type not in types:
+            types.append(collection.type)
+    print("Types of vehicles: ", *types)
+
+    provs = []
+    for collection in collection_list:
+        if collection.provider_name not in provs:
+            provs.append(collection.provider_name)
+    print("Providers of escooters: ", *provs)
+
+    total_dis = 0
+    for collection in collection_list:
+        total_dis += collection.total_distance
+    print("Total Distance: ", total_dis)
+
+    trip_ct = 0
+    for collection in collection_list:
+        trip_ct += len(collection.trip_list)
+    print("Total Trip Count: ", trip_ct)
+
 #Only function to be called in Main method. Performs all parsing, organizing, and analyzing.
 def launch_script():
     collection_list = parse_all_files()
     collection_list = organize_data(collection_list)
     graph_functions(collection_list)
+    statistics(collection_list)
     print("Script is finished.")
 
 def gps_parse(collection_list):
@@ -282,10 +318,6 @@ def helper_gps_parse(gps_str):
 def heatmap(collection_list):
     columns = ["latitude", "longitude"]
     a = []
-
-
-
-
 
 
 #Main Method
